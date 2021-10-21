b6be8dece6764e8ff3aa767fc318a6eecdd8548e
Cuong Tran <cuong@incognito.org>
2021-10-21 17:16:03 +0700

    Handle sell token is PRV

--- estimator.go
+++ estimator.go
@@ -5,6 +5,7 @@ import (
 	"math/big"
 
 	"github.com/cuong-incognito/pdex-v3-fee-estimator/jsonresult"
+	"github.com/incognitochain/incognito-chain/common"
 )
 
 func EstimateFeeInSellToken(
@@ -69,6 +70,9 @@ func EstimatedFeeInPRV(
 	}
 
 	rate := getPriceAgainstPRV(sellToken, pdexState)
+	if sellToken == common.PRVIDStr {
+		rate = [3]*big.Int{new(big.Int).SetInt64(1), new(big.Int).SetInt64(1), new(big.Int).SetInt64(1)}
+	}
 	if rate[2].Uint64() == 0 {
 		return 0, fmt.Errorf("Could not find pair sellToken - PRV")
 	}
